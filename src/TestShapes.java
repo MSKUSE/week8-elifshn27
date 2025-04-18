9de428e8d71d8630f23d338b0f898eebb1fed8a2
Elif Şahin <elifsahin1627@gmail.com>
2025-04-18 12:16:53 +0300

    week 8

---
+++ src/TestShapes.java
@@ -0,0 +1,10 @@
+public class TestShapes {
+    public static void main(String[] args) {
+
+        //Shape shape = new Shape();
+        //shape.describe();
+        Rectangle r1 = new  Rectangle(3,4);
+        Rectangle r2 = new Rectangle(,3,4)
+
+    }
+}
